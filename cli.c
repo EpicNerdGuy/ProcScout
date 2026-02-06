64afa2dd7a5141ec6d27935890366058a9683596
EpicNerdGuy <advikkant13@gmail.com>
2026-02-06 13:36:52 +0530

    added cli banner

--- cli.c
+++ cli.c
@@ -3,9 +3,27 @@
 #include <tchar.h>
 #include <stdio.h>
 
+void print_banner(){
+         printf(
+        "______               _____                 _   \n"
+        "| ___ \\             /  ___|               | |  \n"
+        "| |_/ / __ ___   ___\\ `--.  ___ ___  _   _| |_ \n"
+        "|  __/ '__/ _ \\ / __|`--. \\/ __/ _ \\| | | | __|\n"
+        "| |  | | | (_) | (__/\\__/ / (_| (_) | |_| | |_ \n"
+        "\\_|  |_|  \\___/ \\___\\____/ \\___\\___/ \\__,_|\\__|\n"
+        "                                               \n"
+        "  Lightweight Windows Process Enumeration Tool \n"
+        "                                               \n"
+    );
+}
+
+
+
 int main(){
     HANDLE hProcessSnap;
     PROCESSENTRY32 pe32;
+
+    print_banner();
 
     hProcessSnap = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
     if (hProcessSnap == INVALID_HANDLE_VALUE) {
